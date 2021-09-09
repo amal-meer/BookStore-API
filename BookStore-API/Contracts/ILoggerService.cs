bb036281105b706adb6744bad9828d6297a0764d
AmalMIM333 <am.meer333@gmail.com>
2021-09-09 09:55:55 +0300

    Add project files.

---
+++ BookStore-API/Contracts/ILoggerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.Contracts
+{
+    // The general convention for naming an interface is to put I at the beginning
+    public interface ILoggerService
+    {
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+    }
+}
